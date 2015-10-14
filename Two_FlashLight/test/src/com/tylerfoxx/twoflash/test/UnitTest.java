0211d41a116580ffc91962647b7d0367a8fd1a75
foxxtseng <foxxtseng@msi.com>
2015-10-14 18:03:50 +0800

    [TwoFlashLight] Add UnitTest for jenkins build.

---
+++ Two_FlashLight/test/src/com/tylerfoxx/twoflash/test/UnitTest.java
@@ -0,0 +1,34 @@
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.tylerfoxx.twoflash.MainActivity;
+
+public class UnitTest extends ActivityInstrumentationTestCase2<MainActivity>
+{
+    private MainActivity mMainActivity;
+    
+    public UnitTest(){
+        super(MainActivity.class);
+    }
+    
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        setActivityInitialTouchMode(true);
+        mMainActivity = getActivity();
+    }
+    
+    public void testMainActivityNotNull(){
+        assertNull(mMainActivity);
+    }
+
+
+}
