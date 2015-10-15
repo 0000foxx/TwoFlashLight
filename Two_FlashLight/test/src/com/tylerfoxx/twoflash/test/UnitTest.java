b0fa4c1421cf035f5f2225162dfd5b1849c01372
foxxtseng <foxxtseng@msi.com>
2015-10-15 14:40:21 +0800

    [TwoFlashLight] Add emma for jenkins build.

--- Two_FlashLight/test/src/com/tylerfoxx/twoflash/test/UnitTest.java
+++ Two_FlashLight/test/src/com/tylerfoxx/twoflash/test/UnitTest.java
@@ -27,7 +27,7 @@ public class UnitTest extends ActivityInstrumentationTestCase2<MainActivity>
     }
     
     public void testMainActivityNotNull(){
-        assertNull(mMainActivity);
+        assertNotNull(mMainActivity);
     }
 
 
