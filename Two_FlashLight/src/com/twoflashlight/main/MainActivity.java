40bf684f71012491cc25e99a8b7f1fcae9413960
Jenkins <jenkins@localhost>
2015-10-26 10:44:37 +0800

    [TwoFlashLight] Refactoring method.
    	Refactoring for detBackLight and detFrontLight method.

--- Two_FlashLight/src/com/twoflashlight/main/MainActivity.java
+++ Two_FlashLight/src/com/twoflashlight/main/MainActivity.java
@@ -13,236 +13,249 @@ import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
-import android.os.PowerManager;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.WindowManager.LayoutParams;
 
 import com.twoflashlight.utility.AdmobManager;
 import com.twoflashlight.utility.ScreenWakeLockManager;
-import com.twoflashlight.utility.TraceLogger;
 import com.tylerfoxx.twoflash.R;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
 
-    private Camera mCamera;
-    private Parameters mParameters;
+	private Camera mCamera;
+	private Parameters mParameters;
 
-    private LayoutParams mLayoutParams;
+	private LayoutParams mLayoutParams;
 
-    private int mScreenWidth;
-    private int mScreenHeight;
+	private int mScreenWidth;
+	private int mScreenHeight;
 
-    private int mXCoordinatesOfScreen;
-    private int mYCorrdinatesOfScreen;
-    private int mBackLightIndex;
-    private int mFrontLightIndex;
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        init();
-        
-    }
-    
-    @Override
-    protected void onDestroy()
-    {
-        super.onPause();
-        mCamera.release();
-        mCamera = null;
-    }
-    
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        initCamera();
-        detBackLight();
-        detFrontLight();
-    }
-    
-    private void initCamera()
-    {
-        try {
-            mCamera = Camera.open();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
+	private int mXCoordinatesOfScreen;
+	private int mYCorrdinatesOfScreen;
+	private int mBackLightIndex;
+	private int mFrontLightIndex;
 
-        if (mCamera != null) {
-            mParameters = mCamera.getParameters();
-            mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
-            mCamera.setParameters(mParameters);
-        }
-    }
+	public enum BackLight {
+		CONDITION_INDEX_0(0), CONDITION_INDEX_1(1), CONDITION_INDEX_2(2);
+		private int mIndex;
 
-    private void init()
-    {
-        countBackLightIndex();
-        countFrontLightIndex();
-        AdmobManager.showAdmob(this);
-        ScreenWakeLockManager.makeScreenWakeLock(this);
-        setScreenBrightness(0.1f);
-    }
+		private BackLight(int index) {
+			mIndex = index;
+		}
 
-    private void countFrontLightIndex()
-    {
-        mScreenHeight = (getWindowManager().getDefaultDisplay().getHeight());
-        mYCorrdinatesOfScreen = mScreenHeight / 2;
-        mFrontLightIndex = 5;
-    }
+		public int getNumber() {
+			return mIndex;
+		}
+	}
 
-    private void countBackLightIndex()
-    {
-        mScreenWidth = (getWindowManager().getDefaultDisplay().getWidth());
-        mXCoordinatesOfScreen = mScreenWidth / 2;
-        mBackLightIndex = 1;
-    }
-
-    private void setScreenBrightness(float brightness)
-    {
-        mLayoutParams = getWindow().getAttributes();
-        mLayoutParams.screenBrightness = brightness;
-        getWindow().setAttributes(mLayoutParams);
-    }
 	
-    void detBackLight(){
-    	
-    	ArrayList<Integer> widthal = new ArrayList<Integer>();
-    	
-    	for(int i=0; i<4; i++){
-    		widthal.add(i,mScreenWidth*i/3);
-    	}
-    	
-    	for(int i=0; i<widthal.size()-1; i++){
-    		if(mXCoordinatesOfScreen>=widthal.get(i) && mXCoordinatesOfScreen<=widthal.get(i+1)){
-    			mBackLightIndex = i;
-    		}
-    	}
-    	
-    	TraceLogger.print("backLightIndex:"+mBackLightIndex);
-    	
-        if (mParameters != null) {
-            if (mBackLightIndex == 0) {
-                mParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
-            } else if (mBackLightIndex == 1) {
-                mParameters.setFocusMode("macro");
-                mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-            } else if (mBackLightIndex == 2) {
-                mParameters.setFocusMode("auto");
-                mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
-            }
-            try {
-                // because some system no supply "p.setFocusMode("macro");" so must try{}catch(){}
-                mCamera.setParameters(mParameters);
-            } catch (Exception e) {
+		init();
 
-            }
-        }
-    }
-    
-    void detFrontLight(){
-    	ArrayList<Integer> heightal = new ArrayList<Integer>();
-    	
-    	for(int i=0; i<10; i++){
-    		heightal.add(i,mScreenHeight*i/10);
-    	}
-    	
-    	for(int i=0; i<heightal.size()-1; i++){
-    		if(mYCorrdinatesOfScreen>=heightal.get(i) && mYCorrdinatesOfScreen<=heightal.get(i+1)){
-    			mFrontLightIndex = i;
-    		}
-    	}
-    	
-    	mLayoutParams.screenBrightness = (float)mFrontLightIndex/10;
-    	
-    	if(mLayoutParams.screenBrightness<=0.1f){
-    		mLayoutParams.screenBrightness =0.1f;
-    	}
-    	
-    	TraceLogger.print("lp.screenBrightness:"+mLayoutParams.screenBrightness);
-    	
-    	getWindow().setAttributes(mLayoutParams);
-    	
-    }
-	
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_MOVE:
-                adjustLight(event);
-                break;
-        }
-        return true;
-    }
+	}
 
-    private void adjustLight(MotionEvent event)
-    {
-        getCoordinatesOnScreen(event);
-        detBackLight();
-        detFrontLight();
-    }
+	@Override
+	protected void onDestroy() {
+		super.onPause();
+		mCamera.release();
+		mCamera = null;
+	}
 
-    private void getCoordinatesOnScreen(MotionEvent event)
-    {
-        mXCoordinatesOfScreen = (int) event.getX();
-        mYCorrdinatesOfScreen = (int) event.getY();
-    }
-	
-    private void showNotificationToTitleBar()
-    {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-        Notification notification = new Notification();
-        notification.tickerText = "TwoFlashLight!!!";
-        notification.defaults = Notification.DEFAULT_ALL;
-        notification.setLatestEventInfo(MainActivity.this, "TwoFlashLight", "Welcome to use!!!", pendingIntent);
-        NotificationManager notificatoinManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificatoinManager.notify(0, notification);
-    }
-	
-    @Override
-    public boolean onKeyDown(int keycode, KeyEvent event)
-    {
-        super.onKeyDown(keycode, event);
-        TraceLogger.print("keycode:" + keycode);
-        switch (keycode) {
-            case KeyEvent.KEYCODE_BACK:
-                showExitDialog();
-                return true;
-        }
-        return false;
-    }
-	
-	
-    private void showExitDialog()
-    {
-        Builder dialog = new AlertDialog.Builder(MainActivity.this);
-        dialog.setTitle("Warning");
-        dialog.setMessage("Exit Application??");
-        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initCamera();
+		changeBackLight();
+		changeFrontLight();
+	}
 
-            @Override
-            public void onClick(DialogInterface dialog, int which)
-            {
-                showNotificationToTitleBar();
-                MainActivity.this.finish();
-            }
-        });
-        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+	private void initCamera() {
+		try {
+			mCamera = Camera.open();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
 
-            @Override
-            public void onClick(DialogInterface dialog, int which)
-            {
-            }
-        });
+		if (mCamera != null) {
+			mParameters = mCamera.getParameters();
+			mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+			mCamera.setParameters(mParameters);
+		}
+	}
 
-        dialog.show();
-    }
+	private void init() {
+		initBackLightIndex();
+		initFrontLightIndex();
+		AdmobManager.showAdmob(this);
+		ScreenWakeLockManager.makeScreenWakeLock(this);
+		setScreenBrightness(0.1f);
+	}
+
+	private void initFrontLightIndex() {
+		int frontLightIndexMax = 5;
+		mScreenHeight = (getWindowManager().getDefaultDisplay().getHeight());
+		mYCorrdinatesOfScreen = mScreenHeight / 2;
+		mFrontLightIndex = frontLightIndexMax;
+	}
+
+	private void initBackLightIndex() {
+		mScreenWidth = (getWindowManager().getDefaultDisplay().getWidth());
+		mXCoordinatesOfScreen = mScreenWidth / 2;
+		mBackLightIndex = 1;
+	}
+
+	private void setScreenBrightness(float brightness) {
+		mLayoutParams = getWindow().getAttributes();
+		mLayoutParams.screenBrightness = brightness;
+		getWindow().setAttributes(mLayoutParams);
+	}
+
+	private void changeBackLight() {
+		calculateBackLightIndex();
+		setBackLightFlashMode();
+	}
+
+	private void setBackLightFlashMode() {
+		if (mParameters != null) {
+			if (mBackLightIndex == 0) {
+				mParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+			} else if (mBackLightIndex == 1) {
+				mParameters.setFocusMode("macro");
+				mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+
+			} else if (mBackLightIndex == 2) {
+				mParameters.setFocusMode("auto");
+				mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+			}
+			try {
+				// because some system no supply "p.setFocusMode("macro");" so
+				// must try{}catch(){}
+				mCamera.setParameters(mParameters);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void calculateBackLightIndex() {
+		ArrayList<Integer> screenWidthQuarterIndex = new ArrayList<Integer>();
+		int screenWidthQuarterMax = 4;
+		for (int i = 0; i < screenWidthQuarterMax; ++i) {
+			screenWidthQuarterIndex.add(mScreenWidth * i
+					/ screenWidthQuarterMax - 1);
+		}
+
+		for (int i = 0; i < screenWidthQuarterIndex.size() - 1; ++i) {
+			if (mXCoordinatesOfScreen >= screenWidthQuarterIndex.get(i)
+					&& mXCoordinatesOfScreen <= screenWidthQuarterIndex
+							.get(i + 1)) {
+				mBackLightIndex = i;
+			}
+		}
+	}
+
+	private void changeFrontLight() {
+		calculateFrontLightIndex();
+		setScreenBrightness();
+	}
+
+	private void setScreenBrightness() {
+		float screenBrightnessMin = 0.1f;
+		if (mLayoutParams.screenBrightness <= screenBrightnessMin) {
+			mLayoutParams.screenBrightness = screenBrightnessMin;
+		}
+
+		getWindow().setAttributes(mLayoutParams);
+	}
+
+	private void calculateFrontLightIndex() {
+		ArrayList<Integer> screenHeightQuarterIndex = new ArrayList<Integer>();
+		int screenHeightQuarterMax = 10;
+		for (int i = 0; i < screenHeightQuarterMax; i++) {
+			screenHeightQuarterIndex.add(mScreenHeight * i
+					/ screenHeightQuarterMax);
+		}
+
+		for (int i = 0; i < screenHeightQuarterIndex.size() - 1; i++) {
+			if (mYCorrdinatesOfScreen >= screenHeightQuarterIndex.get(i)
+					&& mYCorrdinatesOfScreen <= screenHeightQuarterIndex
+							.get(i + 1)) {
+				mFrontLightIndex = i;
+			}
+		}
+		mLayoutParams.screenBrightness = (float) mFrontLightIndex
+				/ screenHeightQuarterMax;
+	}
+
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_MOVE:
+			adjustLight(event);
+			break;
+		}
+		return true;
+	}
+
+	private void adjustLight(MotionEvent event) {
+		getCoordinatesOnScreen(event);
+		changeBackLight();
+		changeFrontLight();
+	}
+
+	private void getCoordinatesOnScreen(MotionEvent event) {
+		mXCoordinatesOfScreen = (int) event.getX();
+		mYCorrdinatesOfScreen = (int) event.getY();
+	}
+
+	private void showNotificationToTitleBar() {
+		Intent intent = new Intent(this, MainActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		PendingIntent pendingIntent = PendingIntent.getActivity(
+				MainActivity.this, 0, intent, 0);
+		Notification notification = new Notification();
+		notification.tickerText = "TwoFlashLight!!!";
+		notification.defaults = Notification.DEFAULT_ALL;
+		notification.setLatestEventInfo(MainActivity.this, "TwoFlashLight",
+				"Welcome to use!!!", pendingIntent);
+		NotificationManager notificatoinManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		notificatoinManager.notify(0, notification);
+	}
+
+	@Override
+	public boolean onKeyDown(int keycode, KeyEvent event) {
+		super.onKeyDown(keycode, event);
+		switch (keycode) {
+		case KeyEvent.KEYCODE_BACK:
+			showExitDialog();
+			return true;
+		}
+		return false;
+	}
+
+	private void showExitDialog() {
+		Builder dialog = new AlertDialog.Builder(MainActivity.this);
+		dialog.setTitle("Warning");
+		dialog.setMessage("Exit Application??");
+		dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				showNotificationToTitleBar();
+				MainActivity.this.finish();
+			}
+		});
+		dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
+
+		dialog.show();
+	}
 }
